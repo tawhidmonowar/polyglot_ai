15963fffb6f91722774e1370fe93be06e774a62d
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-04-01 22:45:04 +0600

    Set-up project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
       <Navbar/>
       <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Homepage/>} />
       </Routes>
     </>
   );
