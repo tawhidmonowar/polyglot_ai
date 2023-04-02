758e383bcc7f25c08deb8d11bab9a19d6c8d5562
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-04-02 22:47:26 +0600

    Navbar & Theme Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
 import Navbar from './components/Navbar';
 import Homepage from "./pages/Homepage";
 
 function App() {
+
+  const theme = useMemo(() => createTheme(themeSettings()), []);
+
   return (
     <>
-      <Navbar/>
-      <Routes>
-      <Route path="/" element={<Homepage/>} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
