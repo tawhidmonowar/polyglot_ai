dcd145a3ef0e2f712a8671b132d470714395463d
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-04-09 02:03:21 +0600

    project setup

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const colors = require("colors");
+const dotenv = require("dotenv");
 
 //rest object
 const app = express();
 
+//dotenv
+dotenv.config();
+
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //listen server
-app.listen(8080, () => {
-    console.log("Server runing in dev mood, port 8080");
+app.listen(PORT, () => {
+    console.log(
+      `Server Running in ${process.env.DEV_MODE} mode on port no ${PORT}`
+    );
 });
