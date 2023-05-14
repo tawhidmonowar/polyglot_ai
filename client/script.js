4fb7cdcde579be997c863b0ff8c2a0ac569898a8
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-05-14 18:54:32 +0600

    Update script.js

---
+++ client/script.js
@@ -0,0 +1,13 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("input-code"), {
+	lineNumbers: true,
+	mode: "javascript",
+	theme: "dracula" 
+  });
+
+var outputEditor = CodeMirror.fromTextArea(document.getElementById("output-code"), {
+	lineNumbers: true,
+	mode: "htmlmixed",
+	theme: "dracula",
+	readOnly: true,
+	cursorBlinkRate: 0
+});
