aee8c5fe258983697af40d68d0f1a861dbf1120a
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-05-19 00:31:20 +0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,41 +1,32 @@
 var inputSelect = document.getElementById('input-language');
+var outputSelect = document.getElementById('output-language');
 
 var inputEditor = CodeMirror.fromTextArea(document.getElementById("input-code"), {
 	lineNumbers: true,
-	mode: "text/x-java",
 	theme: "dracula" 
   });
 
 var outputEditor = CodeMirror.fromTextArea(document.getElementById("output-code"), {
 	lineNumbers: true,
-	mode: "javascript",
 	theme: "dracula"
 });
 
-inputSelect.addEventListener('change', function() {
-    var selectedLanguage = inputSelect.value;
-    inputEditor.setOption('mode', 'text/x-'+selectedLanguage);
-    console.log("Input Language xx: " + selectedLanguage + "\n");
-});
-
-
-
-
-
-
 var btnTranslate = document.getElementById("btn-translate");
-
 
 const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    var inputSelect = document.getElementById('input-language');
-    var inputlanguage = inputSelect.options[inputSelect.selectedIndex].value;
-    console.log("Input Language: " + inputlanguage + "\n");
 
-    var outputSelect = document.getElementById('output-language');
-    var outputlanguage = outputSelect.options[outputSelect.selectedIndex].value;
-    console.log("Output Language: " + outputlanguage);
+    var inputlanguage = inputSelect.value;
+    //console.log("Input Language: " + inputlanguage + "\n");
+
+    var outputlanguage = outputSelect.value;
+    //console.log("Output Language: " + outputlanguage);
+
+    var inputCode = inputEditor.getValue();
+    //console.log(x);
+
+    var final_prompt = "##### Translate this code from "+ inputlanguage + " into " + outputlanguage + "\n### "+ inputlanguage +"\n    \n    " + inputCode + "\n    \n### " + outputlanguage;
+    console.log(final_prompt);
 
 }
 
