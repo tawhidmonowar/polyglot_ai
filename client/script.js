e9384d04ac373525bdf10119da6b96e257d888de
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-05-19 15:17:46 +0600

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,5 +1,8 @@
 var inputSelect = document.getElementById('input-language');
 var outputSelect = document.getElementById('output-language');
+var btnTranslate = document.getElementById("btn-translate");
+var inputCopy = document.getElementById('btn-input-copy');
+var outputCopy = document.getElementById('btn-output-copy');
 
 var inputEditor = CodeMirror.fromTextArea(document.getElementById("input-code"), {
 	lineNumbers: true,
@@ -11,7 +14,6 @@ var outputEditor = CodeMirror.fromTextArea(document.getElementById("output-code"
 	theme: "dracula"
 });
 
-var btnTranslate = document.getElementById("btn-translate");
 
 const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,9 +23,35 @@ const handleSubmit = async (e) => {
     var inputCode = inputEditor.getValue();
     var final_prompt = "##### Translate this code from "+ inputlanguage + " into " + outputlanguage + "\n### "+ inputlanguage +"\n    \n    " + inputCode + "\n    \n### " + outputlanguage;
     console.log(final_prompt);
-
     outputEditor.setValue(final_prompt);
-
 }
 
 btnTranslate.addEventListener('click', handleSubmit);
+
+inputCopy.addEventListener('click', copyInputCode);
+inputCopy.addEventListener('mouseout', outFunc);
+
+outputCopy.addEventListener('click', copyOutputCode);
+outputCopy.addEventListener('mouseout', outFunc);
+
+
+function copyInputCode() {
+  var inputCode = inputEditor.getValue();
+  navigator.clipboard.writeText(inputCode);
+  var tooltip = document.getElementById("input-tooltip");
+  tooltip.innerHTML = "Copied";
+}
+
+function copyOutputCode() {
+  var outputCode = outputEditor.getValue();
+  navigator.clipboard.writeText(outputCode);
+  var tooltip = document.getElementById("output-tooltip");
+  tooltip.innerHTML = "Copied";
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("output-tooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+  var tooltip = document.getElementById("input-tooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
