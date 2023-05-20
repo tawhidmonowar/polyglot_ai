cf8c0fea227a650844359716ce89b344690ba545
Tawhid Monowar <tawhidmonowar@gmail.com>
2023-05-21 00:31:37 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/', async (req, res) => {
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0,
-      max_tokens: 150,
+      max_tokens: 300,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0
